44421c9129375a9ab8d4a647677a36e2f0532797
Aniruddha Chakraborty <aniruddha11chakraborty@gmail.com>
2024-05-31 20:11:20 +0530

    Week 2 2nd commit

--- Week 2/simple_calculator.py
+++ Week 2/simple_calculator.py
@@ -23,14 +23,7 @@ def div(num1, num2):
     return num1 / num2
 
 
-print("Please select operation -\n"
-      "1. Add\n"
-      "2. Subtract\n"
-      "3. Multiply\n"
-      "4. Divide\n")
-
-
-# Take input from the user
+# Menu
 while True:
     select = int(input(
         "Select operations form 1.Add, 2. Subtract, 3. Multiply, 4. Divide, 5. Exit :"))
